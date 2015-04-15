d9e90eff5968e7cc9f64a215b2670e63e128edfd
XiumingXU <gracexuxiuming@gmail.com>
2015-04-15 02:07:35 +0200

    开始语音识别

--- AudioViewController.h
+++ AudioViewController.h
@@ -5,9 +5,18 @@
 //  Created by GraceXu on 14/04/15.
 //
 //
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
 
-#import "ViewController.h"
+@interface AudioViewController : UIViewController<AVAudioRecorderDelegate>
+{
+    AVAudioRecorder *recorder;
+    NSTimer *timer;
+    NSURL *urlPlay;
+    NSString * zodiacName;
+    
+}
 
-@interface AudioViewController : ViewController
 
 @end
